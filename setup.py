2e20a1fbf16fe7019311160c11d9532da4197208
PhuTran1005 <phutx2000@kaist.ac.kr>
2024-03-30 19:43:49 +0900

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = 'WasteDetection',
+    version = '0.0.0',
+    author = 'Tran Dinh Phu',
+    author_email = 'phutx2000@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
